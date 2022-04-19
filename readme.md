307ebc8b3d77496173ca4c81ad5bdbbb7f467bea
huggy <i@huggy.moe>
2022-04-19 17:14:46 +0800

    update readme

---
+++ readme.md
@@ -0,0 +1,13 @@
+# webpagebot api
+[@WebpageBot](https://t.me/WebpageBot) can update link's cache in Telegram, but not have web api.  
+So We can create a userbot and send link to @WebpageBot to update cache.
+
+
+Simple code, no documentation.
+
+You can get apiId / apiHash from [my.telegram.org](https://my.telegram.org).
+
+See more:  
+
+https://github.com/Bannerets/tdl
+
--- src/app.ts
+++ src/app.ts
@@ -7,19 +7,20 @@ import { TDLib } from 'tdl-tdlib-addon'
 const app = new Koa()
 
 const client = new Client(new TDLib(), {
-  apiId: 111111,
-  apiHash: '',
+  apiId: 179502,
+  apiHash: 'fb9edfa0448a501fc57f3cdd19739a8a',
 })
 
 app.use(KoaBodyparser())
 app.use(async (ctx) => {
   let url = (ctx.request.body && ctx.request.body.url) ? ctx.request.body.url : ctx.request.url.substr(1)
+  console.log(new Date(), url)
   if (url) {
     try {
       new URL(url)
       await client.invoke({
         _: 'sendMessage',
-        chat_id: 169642392,
+        chat_id: 169642392, // @webpagebot
         input_message_content: {
           _: 'inputMessageText',
           text: {
@@ -43,7 +44,7 @@ app.use(async (ctx) => {
 const tg_initial = async () => {
   await client.connect()
   await client.login()
-  http.createServer(app.callback()).listen(2344, '192.168.191.41')
+  http.createServer(app.callback()).listen(2344, '127.0.0.1')
   console.log('http server listening on 2344')
 }
 tg_initial()
